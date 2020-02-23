f0caf82a6a5005ae0f58c7671af523784a4baf3b
Alejandro Moreno <alejandromorenoastorga@gmail.com>
2020-02-23 18:39:17 +0100

    Create AEMET-opendata.py

---
+++ AEMET-opendata/AEMET-opendata.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 18:01:47 2020
+
+@author: Alex
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 16:52:25 2020
+
+@author: Alex
+"""
+
+import requests as req
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+api_key = config['AEMET-opendata']['api_key']
+print(api_key)
+
+
+start_date, end_date = '2016-01-01T00:00:00UTC', '2020-01-02T00:00:00UTC'
+station = '8019'
+
+url = 'https://opendata.aemet.es/opendata/api/valores/climatologicos/diarios/datos/fechaini/'+start_date+'/fechafin/'+end_date+'/estacion/'+station
+
+querystring = {"api_key":api_key}
+
+headers = {
+    'cache-control': "no-cache"
+    }
+
+response = req.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+
+json_response =response.json()
+print(json_response['datos'])
+df = pd.read_json (json_response['datos'],convert_dates=True)
+
+df['fecha'] = pd.to_datetime(df['fecha'])
+df['tmed'] = df['tmed'].str.replace(',', '.').astype(float)
+df['tmin'] = df['tmin'].str.replace(',', '.').astype(float)
+df['tmax'] = df['tmax'].str.replace(',', '.').astype(float)
+
+df['tdif'] = df['tmax'] - df['tmin']
+
+print(df.dtypes)
+print(df.head())
+
+aemet_daily = df.set_index('fecha')
+print(aemet_daily.head())
+aemet_daily['Year'] = aemet_daily.index.year
+aemet_daily['Month'] = aemet_daily.index.month
+aemet_daily['Weekday Name'] = aemet_daily.index.day_name()
+print(aemet_daily.head())
+
+
+# Use seaborn style defaults and set the default figure size
+sns.set(rc={'figure.figsize':(11, 4)})
+aemet_daily['tmed'].plot(marker='.', alpha=0.5, linestyle='None',linewidth=0.5);
+
+cols_plot = ['tmed', 'tmin', 'tmax']
+    
+ 
+fig, axes = plt.subplots(len(cols_plot), 1, figsize=(11, 10), sharex=True)
+
+for name, ax in zip(['tmed', 'tmin', 'tmax'], axes):    
+    sns.boxplot(data=aemet_daily, x='Month', y=name, ax=ax)
+    ax.set_ylabel('Temperature (ºC)')    
+    ax.set_title(name)
+# Remove the automatic x-axis label from all but the bottom subplot
+if ax != axes[-1]:
+    ax.set_xlabel('')    
+    
+fig, ax = plt.subplots()    
+sns.boxplot(data=aemet_daily, x='Month', y='tdif', ax=ax)
+ax.set_ylabel('Difference (Max-Min)')
+ax.set_title('tdif')
+    
+    
+# Specify the data columns we want to include (i.e. exclude Year, Month, Weekday Name)
+data_columns = ['tmed', 'tmin', 'tmax','tdif']
+# Resample to weekly frequency, aggregating with mean
+aemet_weekly_mean = aemet_daily[data_columns].resample('W').mean()
+aemet_weekly_mean.head(5)
+
+# Start and end of the date range to extract
+start, end = '2019-01', '2019-6'
+# Plot daily and weekly resampled time series together
+fig, ax = plt.subplots()
+ax.plot(aemet_daily.loc[start:end, 'tmed'],
+marker='.', linestyle='-', linewidth=0.5, label='Daily')
+ax.plot(aemet_weekly_mean.loc[start:end, 'tmed'],
+marker='o', markersize=8, linestyle='-', label='Weekly Mean Resample')
+ax.set_ylabel('Temperature (ºC)')
+ax.legend();
+
+# Compute the monthly sums, setting the value to NaN for any month which has
+# fewer than 28 days of data
+aemet_monthly_mean = aemet_daily[data_columns].resample('M').mean()
+aemet_monthly_mean.head(5)
+
+fig, ax = plt.subplots()
+ax.plot(aemet_monthly_mean['tmed'], color='black', label='tmed')
+aemet_monthly_mean[['tmin', 'tmax']].plot.area(ax=ax, linewidth=0)
+ax.xaxis.set_major_locator(mdates.YearLocator())
+ax.legend()
+ax.set_ylabel('Temperature (ºC)');
+
